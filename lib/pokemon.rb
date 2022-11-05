5a372fd3ad0a691a39cd823cc19cba4dd0807f3c
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-05 10:09:06 +0300

    completed assignment

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,39 +8,26 @@ class Pokemon
         @type = type
         @db = db
     end
-    def self.create_table
-        sql =  <<-SQL
-          CREATE TABLE IF NOT EXISTS pokemons (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            type TEXT
-            )
-            SQL
-        DB[:conn].execute(sql)
-      end
-      def save
-        sql = <<-SQL
-          INSERT INTO pokemons (name, type)
-          VALUES (?, ?)
-        SQL
-    
-        DB[:conn].execute(sql, self.name, self.type)
-    
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemons")[0][0]
-    
-        self
-    
-      end
-      def self.create(name:, type:)
-        pokemon = Pokemon.new(name: name, type: type)
-        pokemon.save
-      end
-      def self.find
-        sql = <<-SQL 
-        
-        SQL
-      end
-end
 
-binding.pry
-"pls"
+    def self.save(name, type, db)
+
+      sql = <<-SQL
+      INSERT INTO Pokemon (name, type) VALUES (?, ?)
+      SQL
+
+      db.execute(sql, name, type)
+
+  end
+
+  def self.find(id, db)
+
+      sql = <<-SQL
+      SELECT * FROM Pokemon WHERE id = ?
+      SQL
+
+      poke = db.execute(sql, id).flatten
+      self.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+
+  end
+
+end
